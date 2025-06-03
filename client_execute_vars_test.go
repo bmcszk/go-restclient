2c7f499e8b59140b6170554e3f11e66f2d23d1fc
Błażej Mcszk <dzikun@gmail.com>
2025-06-04 00:08:55 +0200

    test(http-client): Add test for in-place var in body (TNEW3)
    - Added 'variable_substitution_in_body' sub-test to
      TestExecuteFile_InPlaceVariables in client_execute_vars_test.go.
    - This test verifies that in-place variables are correctly
      substituted into JSON request bodies.

--- client_execute_vars_test.go
+++ client_execute_vars_test.go
@@ -1836,6 +1836,43 @@ X-Custom-Header: {{my_header_value}}
 		assert.Equal(t, "secret-token", capturedHeaderValue, "The X-Custom-Header should be correctly substituted")
 	})
 
+	t.Run("variable_substitution_in_body", func(t *testing.T) {
+		// Given: an .http file with an in-place variable used in a JSON request body
+		var capturedBody []byte
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var err error
+			capturedBody, err = io.ReadAll(r.Body)
+			require.NoError(t, err)
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer server.Close()
+
+		fileContent := fmt.Sprintf(`
+@user_id = user123
+
+### Test Request With Body Var
+POST %s/users
+Content-Type: application/json
+
+{
+  "id": "{{user_id}}",
+  "status": "active"
+}
+`, server.URL)
+
+		requestFilePath := createTempHTTPFileFromString(t, fileContent)
+
+		client, err := NewClient()
+		require.NoError(t, err)
+
+		// When: the .http file is executed
+		_, execErr := client.ExecuteFile(context.Background(), requestFilePath)
+
+		// Then: no error should occur and the body should be substituted correctly
+		require.NoError(t, execErr, "ExecuteFile should not return an error")
+		assert.JSONEq(t, `{"id": "user123", "status": "active"}`, string(capturedBody), "The request body should be correctly substituted")
+	})
+
 	// TODO: Add more sub-tests for other scenarios:
 	// - variable in header
 	// - variable in body
