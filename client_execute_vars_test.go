3bcb982f6dea02c0da448ba54ff0166488882977
Błażej Mcszk <dzikun@gmail.com>
2025-06-04 00:08:29 +0200

    test(http-client): Add test for in-place var in header (TNEW3)
    - Added 'variable_substitution_in_header' sub-test to
      TestExecuteFile_InPlaceVariables in client_execute_vars_test.go.
    - This test verifies that in-place variables are correctly
      substituted into request headers.

--- client_execute_vars_test.go
+++ client_execute_vars_test.go
@@ -1806,6 +1806,36 @@ GET {{host}}/expected_path
 		assert.Equal(t, "/expected_path", capturedURLPath, "The request path should match, indicating in-place var was used")
 	})
 
+	t.Run("variable_substitution_in_header", func(t *testing.T) {
+		// Given: an .http file with an in-place variable used in a header
+		var capturedHeaderValue string
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			capturedHeaderValue = r.Header.Get("X-Custom-Header")
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer server.Close()
+
+		fileContent := fmt.Sprintf(`
+@my_header_value = secret-token
+
+### Test Request With Header Var
+GET %s/somepath
+X-Custom-Header: {{my_header_value}}
+`, server.URL)
+
+		requestFilePath := createTempHTTPFileFromString(t, fileContent)
+
+		client, err := NewClient()
+		require.NoError(t, err)
+
+		// When: the .http file is executed
+		_, execErr := client.ExecuteFile(context.Background(), requestFilePath)
+
+		// Then: no error should occur and the header should be substituted correctly
+		require.NoError(t, execErr, "ExecuteFile should not return an error")
+		assert.Equal(t, "secret-token", capturedHeaderValue, "The X-Custom-Header should be correctly substituted")
+	})
+
 	// TODO: Add more sub-tests for other scenarios:
 	// - variable in header
 	// - variable in body
