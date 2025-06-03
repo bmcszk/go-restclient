da44e62f45ea392650ce0ba856a0d02fb335ad80
Błażej Mcszk <dzikun@gmail.com>
2025-06-04 01:01:31 +0200

    feat(test): Add test for in-place variable in body (TNEW3)
    This commit adds a new sub-test 'inplace_variable_in_body' to
    TestExecuteFile_InPlaceVariables to verify that in-place variables
    are correctly substituted when used in the request body.

--- client_execute_vars_test.go
+++ client_execute_vars_test.go
@@ -1987,8 +1987,49 @@ GET %s/some/path
 		assert.Equal(t, headerValue, capturedHeaders.Get(headerKey), "The header should be correctly substituted with the in-place variable")
 	})
 
+	t.Run("inplace_variable_in_body", func(t *testing.T) {
+		// Given: an .http file with an in-place variable used in the request body
+		const userIdValue = "user-from-var-456"
+		const expectedBody = `{"id": "user-from-var-456", "status": "pending"}`
+
+		var capturedBody []byte
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var err error
+			capturedBody, err = io.ReadAll(r.Body)
+			require.NoError(t, err)
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer server.Close()
+
+		fileContent := fmt.Sprintf(`
+@my_user_id = %s
+
+### Test Request With In-Place Var in Body
+POST %s/submit
+Content-Type: application/json
+
+{
+  "id": "{{my_user_id}}",
+  "status": "pending"
+}
+`, userIdValue, server.URL)
+
+		requestFilePath := createTempHTTPFileFromString(t, fileContent)
+
+		client, err := NewClient()
+		require.NoError(t, err)
+
+		// When: the .http file is executed
+		results, execErr := client.ExecuteFile(context.Background(), requestFilePath)
+
+		// Then: no error should occur and the body should be correctly substituted
+		require.NoError(t, execErr, "ExecuteFile should not return an error")
+		require.Len(t, results, 1, "Should have one result")
+		require.Nil(t, results[0].Error, "Request execution error should be nil")
+		assert.JSONEq(t, expectedBody, string(capturedBody), "The request body should be correctly substituted with the in-place variable")
+	})
+
 	// TODO: Add more sub-tests for other scenarios:
-	// - variable in body
 	// - variable defined with another variable (e.g., @var2 = {{var1}})
 	// - variable defined with system variable (e.g., @var_uuid = {{$uuid}}) // This is partially covered
 	// - variable defined with OS env variable (e.g., @var_env = {{$env.MY_TEST_VAR}}) // This is partially covered by the fixed test
