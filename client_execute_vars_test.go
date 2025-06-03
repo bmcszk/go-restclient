a966a35e2f6c9337ad3778ecc75aada77070e016
Błażej Mcszk <dzikun@gmail.com>
2025-06-04 01:10:11 +0200

    feat(client): Add test for in-place var with .VAR_NAME OS var (TNEW3)

--- client_execute_vars_test.go
+++ client_execute_vars_test.go
@@ -2103,7 +2103,42 @@ GET %s/some/path
 		assert.NotEmpty(t, capturedHeaderValue, "Captured UUID header should not be empty")
 	})
 
+	t.Run("inplace_variable_defined_by_dot_env_os_variable", func(t *testing.T) {
+		// Given: an .http file with an in-place variable defined by an OS environment variable using {{$env.VAR_NAME}}
+		const testEnvVarName = "MY_CONFIG_PATH_TEST_DOT_ENV"
+		const testEnvVarValue = "/usr/local/appconfig_dotenv" // Using a value that starts with /
+		var capturedURLPath string
+
+		t.Setenv(testEnvVarName, testEnvVarValue)
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			capturedURLPath = r.URL.Path
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer server.Close()
+
+		fileContent := fmt.Sprintf(`
+@my_path_from_env = {{$env.%s}}
+
+### Test Request With OS Env Var ({{$env.VAR}}) In In-Place Var
+GET %s{{my_path_from_env}}/data
+`, testEnvVarName, server.URL)
+
+		requestFilePath := createTempHTTPFileFromString(t, fileContent)
+
+		client, err := NewClient()
+		require.NoError(t, err)
+
+		results, execErr := client.ExecuteFile(context.Background(), requestFilePath)
+
+		require.NoError(t, execErr, "ExecuteFile should not return an error")
+		require.Len(t, results, 1, "Should have one result")
+		require.Nil(t, results[0].Error, "Request execution error should be nil")
+
+		expectedPath := testEnvVarValue + "/data"
+		assert.Equal(t, expectedPath, capturedURLPath, "The URL path should be correctly substituted with the OS environment variable via {{$env.VAR_NAME}} in-place var")
+	})
+
 	// TODO: Add more sub-tests for other scenarios:
-	// - variable defined with OS env variable (e.g., @var_env = {{$env.MY_TEST_VAR}}) // This is partially covered by the fixed test
 	// - malformed definitions (e.g., @name_no_value, @=value)
 }
