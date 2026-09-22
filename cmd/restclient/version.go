d00987c3850e3f0b5602fe8b8188d4c40ddc6ca5
Blaze Mcszk <dzikun@gmail.com>
2026-09-22 19:33:03 +0200

    fix(cli): name vcsInfo results (revive confusing-results on CI lint)

--- cmd/restclient/version.go
+++ cmd/restclient/version.go
@@ -25,7 +25,7 @@ func buildVersion() string {
 }
 
 // vcsInfo returns the embedded vcs revision and dirty flag, if any.
-func vcsInfo(info *debug.BuildInfo) (string, string) {
+func vcsInfo(info *debug.BuildInfo) (rev, dirty string) {
 	rev, dirty := "", ""
 	for _, s := range info.Settings {
 		switch s.Key {
